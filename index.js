b0da7464fe9a278f7eccb360a21ce617980c63ef
Hamza Sajid <github@hamzasajid.com>
2021-09-13 15:51:53 -0700

    task 1 and 2 complete

--- index.js
+++ index.js
@@ -28,11 +28,17 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
+
+  Both counters are adding 1 to the score when they are called but counter1 will reset to zero each time. 
   
   2. Which of the two uses a closure? How can you tell?
+
+  Counter1 because it is using a function within a function and reaching for a variable declared outside of its scope. 
   
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
+
+     counter1 would be preferable to reset the count to zero each time and counter 2 for incrementing by one each time. 
 */
 
 // counter1 code
@@ -44,6 +50,7 @@ function counterMaker() {
 }
 
 const counter1 = counterMaker();
+
 
 // counter2 code
 let count = 0;
@@ -62,9 +69,14 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(){
+    return Math.floor(Math.random()*3);
 }
+
+for (let i=0; i<15; i++) {
+  console.log(inning());
+}
+
 
 
 /* ⚾️⚾️⚾️ Task 3: finalScore() ⚾️⚾️⚾️
